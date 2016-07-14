f15b6652818f82604547641274a2fb86febb51e8
Sahil Dhawan <sahil@sahils-macbook-air.local>
2016-07-15 04:03:29 +0530

    Initial Commit

---
+++ TouchDownDemo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  TouchDownDemo
+//
+
+//  Copyright (c) 2016 Sahil Dhawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ TouchDownDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TouchDownDemo
+//
+//  Created by Sahil Dhawan on 15/07/16.
+//  Copyright © 2016 Sahil Dhawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
